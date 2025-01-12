ee3287804deec40e035e6dd0cf05e26e22092362
Divyesh Pansuriya <adpansuriya7300@gmail.com>
2025-01-12 19:45:41 +0530

    Initial Commit

---
+++ RDLiquidSpreadButton/RDLiquidSpreadButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  RDLiquidSpreadButtonApp.swift
+//  RDLiquidSpreadButton
+//
+//  Created by Divyesh Pansuriya on 12/01/25.
+//
+
+import SwiftUI
+
+@main
+struct RDLiquidSpreadButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
